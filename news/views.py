c7341f00878db99b00081532be26bbcd321c4262
cesvh <cesar.vega.hdez@gmail.com>
2023-02-26 11:53:21 -0600

    Vista de Reporteros y Artículos
    Se agrega vista y url de Reporteros
    Se agrega vista y url de Artículos

--- news/views.py
+++ news/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 def index(request):
-	context = {'title': 'Artículos publicados'}
+	context = {'title': 'Reporteros y Artículos'}
 	return render(request, 'news/index.html', context)
 
+def reporter(request):
+	reporters = models.Reporter.objects.all()
+	context = {'title': 'Reporteros', 'reporters': reporters}
+	return render(request, 'news/reporters.html', context)
+	
