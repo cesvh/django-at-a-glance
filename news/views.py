1e28cee8e2bd7b4753481d17cfd752b382fc652e
cesvh <cesar.vega.hdez@gmail.com>
2023-02-26 11:03:17 -0600

    Vistas
    Se configura las vistas, plantillas y la url

--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	context = {'title': 'Artículos publicados'}
+	return render(request, 'news/index.html', context)
+
